5e516e2c064e37d5c56f69da1c060d7571b93d27
latyr0503 <latyr0503@gmail.com>
2024-09-06 12:53:37 +0000

    reception de mail

--- src/Elements/FormModal.jsx
+++ src/Elements/FormModal.jsx
@@ -1,41 +1,50 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function FormModal() {
-  // États pour les champs du formulaire
+  // États pour chaque champ du formulaire
   const [motif, setMotif] = useState("");
-  const [docteur, setDocteur] = useState("");
+  const [doctor, setDoctor] = useState("");
+  const [patient, setPatient] = useState("");
   const [date, setDate] = useState("");
   const [heure, setHeure] = useState("");
-  const [nomComplet, setNomComplet] = useState("");
-  const [numero, setNumero] = useState("");
   const [email, setEmail] = useState("");
+  const [numero, setNumero] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // Nouvel état pour le loader
 
   // Fonction pour gérer la soumission du formulaire
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Empêche le rechargement de la page
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    // Créer un objet avec les données du formulaire
-    const formData = {
-      motif,
-      docteur,
-      date,
-      heure,
-      nomComplet,
-      numero,
-      email,
-    };
-
-    // Enregistrer les données dans le localStorage
-    localStorage.setItem("rendezVous", JSON.stringify(formData));
-
-    // Optionnel: Réinitialiser les champs du formulaire après l'enregistrement
-    setMotif("");
-    setDocteur("");
-    setDate("");
-    setHeure("");
-    setNomComplet("");
-    setNumero("");
-    setEmail("");
+    try {
+      // Envoyer les données du formulaire à l'API
+      await axios.post(
+        "https://prise-de-rv-backend-nestjs.onrender.com/rendezvous",
+        {
+          motif,
+          doctor,
+          patient,
+          date,
+          heure,
+          email,
+          numero,
+        },
+        setIsLoading(true) // Démarre le loader
+      );
+      setMotif("");
+      setDoctor("");
+      setPatient("");
+      setDate("");
+      setHeure("");
+      setEmail("");
+      setNumero("");
+      setIsLoading(false); // Arrête le loader une fois la requête réussie
+      alert("Rendez-vous ajouté avec succès!");
+    } catch (error) {
+      console.error("Erreur lors de l'ajout du rendez-vous", error);
+      alert("Erreur lors de l'ajout du rendez-vous");
+      setIsLoading(false); // Arrête le loader une fois la requête réussie
+    }
   };
 
   return (
@@ -59,8 +68,8 @@ export default function FormModal() {
           </div>
           <select
             className="rounded-lg bg-white text-slate-800 border border-slate-800 p-3"
-            value={docteur}
-            onChange={(e) => setDocteur(e.target.value)}
+            value={doctor}
+            onChange={(e) => setDoctor(e.target.value)}
           >
             <option disabled value="">
               Selectionner un docteur
@@ -100,8 +109,8 @@ export default function FormModal() {
             type="text"
             className="w-full rounded-lg bg-white text-slate-800 border border-slate-800 p-3"
             placeholder="Nom Complet du patient"
-            value={nomComplet}
-            onChange={(e) => setNomComplet(e.target.value)}
+            value={patient}
+            onChange={(e) => setPatient(e.target.value)}
           />
         </div>
         <div>
@@ -124,12 +133,20 @@ export default function FormModal() {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <button
-          type="submit"
-          className="bg-blue-700 text-white p-4 rounded-lg uppercase font-bold"
-        >
-          envoyer
-        </button>
+        {isLoading ? (
+          <div className="flex flex-row justify-center gap-2">
+            <div className="w-4 h-4 rounded-full bg-sky-500 animate-bounce [animation-delay:.7s]"></div>
+            <div className="w-4 h-4 rounded-full bg-sky-500 animate-bounce [animation-delay:.3s]"></div>
+            <div className="w-4 h-4 rounded-full bg-sky-500 animate-bounce [animation-delay:.7s]"></div>
+          </div>
+        ) : (
+          <button
+            type="submit"
+            className="w-full py-3 text-white duration-150 bg-sky-500 rounded-lg hover:bg-sky-700 active:shadow-lg my-5"
+          >
+            Enregistrer
+          </button>
+        )}
       </div>
     </form>
   );
