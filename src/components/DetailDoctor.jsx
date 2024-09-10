ad66547c58435f61fc48ba4c0d72a75973ba1463
latyr0503 <latyr0503@gmail.com>
2024-09-10 10:18:45 +0000

    recup travail khoudia

--- src/components/DetailDoctor.jsx
+++ src/components/DetailDoctor.jsx
@@ -31,8 +31,10 @@ const DetailDoctor = () => {
         <div className="flex flex-col">
           <img src={medecin} alt="medecin" className="w-80 md:w-72 lg:w-72" />
           <h3 className="mt-8 font-medium">Jordan Kelley</h3>
-          <h6 className="text-xs text-blue-700 border-b border-gray-300 pb-2 font-normal">Cardiology</h6>
-          <div className="mt-4 flex items-center text-gray-800 text-xs" >
+          <h6 className="text-sm text-blue-700 border-b border-gray-300 pb-2 font-normal">
+            Cardiology
+          </h6>
+          <div className="mt-4 flex items-center text-gray-800">
             <Phone className="mr-2 text-blue-700 size-4" />
             <span>(+221) 783828958</span>
           </div>
@@ -43,21 +45,23 @@ const DetailDoctor = () => {
         </div>
 
         <button
-          className="mt-6 px-4 py-2 bg-cyan-300 w-72 md:w-64 lg:w-72 text-white rounded-lg hover:bg-cyan-400 flex items-center justify-center"
+          className="mt-6 px-4 py-2 bg-blue-700 w-72 md:w-64 lg:w-72 text-white rounded-lg hover:bg-blue-500 flex items-center justify-center"
           onClick={handleOpenModal}
         >
           <Calendar className="mr-2" />
           Book Appointment
         </button>
         {isModalOpen && (
-          <div className="mt-2 w-72 md:w-64 lg:w-72 bg-cyan-700 p-6 rounded-lg bg-cyan-700 shadow-lg shadow-cyan-500/50">
-            <h6 className="text-white">We are here for you</h6>
-            <h2 className="text-lg font-bold mb-4 text-white">Book Appointment</h2>
+          <div className="mt-2 w-72 md:w-64 lg:w-72 bg-blue-700 p-6 rounded-lg ">
+            <h6 className="text-white">Nous sommes là pour vous</h6>
+            <h2 className="text-lg font-bold mb-4 text-white">
+              Prendre un rendez-vous
+            </h2>
             <form>
               <div className="mb-4">
                 <input
                   type="text"
-                  className="border rounded w-full py-2 px-3 bg-white"
+                  className="border rounded-lg w-full py-2 px-3 bg-white"
                   placeholder="Your full name"
                   required
                 />
@@ -65,7 +69,7 @@ const DetailDoctor = () => {
               <div className="mb-4">
                 <input
                   type="tel"
-                  className="border rounded w-full py-2 px-3 bg-white"
+                  className="border rounded-lg w-full py-2 px-3 bg-white"
                   placeholder="Phone"
                   required
                 />
@@ -73,22 +77,22 @@ const DetailDoctor = () => {
               <div className="mb-4">
                 <input
                   type="date"
-                  className="border rounded w-full py-2 px-3 bg-white"
+                  className="border rounded-lg w-full py-2 px-3 bg-white"
                   required
                 />
               </div>
               <div className="mb-4">
                 <input
                   type="time"
-                  className="border rounded w-full py-2 px-3 bg-white"
+                  className="border rounded-lg w-full py-2 px-3 bg-white"
                   required
                 />
               </div>
               <button
                 type="submit"
-                className="bg-cyan-300 text-white px-4 py-2 rounded-lg flex items-center w-full gap-2 hover:bg-cyan-600 transition-transform transform hover:translate-y-1"
+                className="bg-white text-sm font-bold text-blue-700 px-4 py-2 rounded-lg flex items-center w-full gap-2 transition-transform transform hover:translate-y-1"
               >
-                Book Appointment
+                Prendre un rendez-vous
                 <svg
                   className="w-4 h-4 transition-transform transform group-hover:translate-x-2"
                   stroke="currentColor"
@@ -108,12 +112,11 @@ const DetailDoctor = () => {
           </div>
         )}
 
-        <div className="flex justify-center items-center my-4">
+        <div className="flex  items-center my-4">
           <a
             href={doctor.twitter}
             className="mx-2 text-gray-500 hover:text-blue-600"
             target="_blank"
-            rel="noopener noreferrer"
           >
             <Twitter />
           </a>
@@ -121,7 +124,6 @@ const DetailDoctor = () => {
             href={doctor.facebook}
             className="mx-2 text-gray-500 hover:text-blue-600"
             target="_blank"
-            rel="noopener noreferrer"
           >
             <Facebook />
           </a>
@@ -129,7 +131,6 @@ const DetailDoctor = () => {
             href={doctor.instagram}
             className="mx-2 text-gray-500 hover:text-blue-600"
             target="_blank"
-            rel="noopener noreferrer"
           >
             <Instagram />
           </a>
@@ -137,50 +138,70 @@ const DetailDoctor = () => {
             href={doctor.linkedin}
             className="mx-2 text-gray-500 hover:text-blue-600"
             target="_blank"
-            rel="noopener noreferrer"
           >
             <Linkedin />
           </a>
         </div>
       </div>
-      <div className="md:w-2/3 p-4">
-        <p className="text-xl tracking-wide font-light">
-          Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo non habent claritatem insitamconsequat duis autem facilisis at vero eros vel eum iriure. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum
-        </p><br />
-        <p className="font-medium text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Ulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril.
-
+      <div className="md:w-2/3 px-5 ">
+        <p className="text-xl text-justify font-light">
+          Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+          commodo non habent claritatem insitamconsequat duis autem facilisis at
+          vero eros vel eum iriure. Duis autem vel eum iriure dolor in hendrerit
+          in vulputate velit esse molestie consequat, vel illum dolore eu
+          feugiat nulla facilisis at vero eros et accumsan et iusto odio
+          dignissim qui blandit praesent luptatum
+        </p>
+        <br />
+        <p className="font-medium text-justify text-sm">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
+          vero eros et accumsan et iusto odio dignissim qui blandit praesent
+          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          Nam liber tempor cum soluta nobis eleifend option congue nihil
+          imperdiet doming id quod mazim placerat facer possim assum. Nam liber
+          tempor cum soluta nobis eleifend option congue nihil imperdiet doming
+          id quod mazim placerat facer possim assum. Typi non habent claritatem
+          insitam; est usus legentis in iis qui facit eorum claritatem.
+          Investigationes demonstraverunt lectores legere me lius quod ii legunt
+          saepius. Claritas est etiam processus dynamicus, qui sequitur
+          mutationem consuetudium lectorum. Ulputate velit esse molestie
+          consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
+          accumsan et iusto odio dignissim qui blandit praesent luptatum zzril.
         </p>
         <div className="mt-6">
-          <div className="mt-6">
-            <div className="flex justify-between border-b border-gray-300 pb-2">
-              <span className="font-semibold">Specialty:</span>
-              <span>Cardiology</span>
-            </div>
-            <div className="flex justify-between border-b border-gray-300 pb-2 mt-2">
-              <span className="font-semibold">Degrees:</span>
-              <span>M.D. of Medicine</span>
-            </div>
-            <div className="flex justify-between border-b border-gray-300 pb-2 mt-2">
-              <span className="font-semibold">Training:</span>
-              <span>Lorem ipsum dolor sit...</span>
-            </div>
-            <div className="flex justify-between border-b border-gray-300 pb-2 mt-2">
-              <span className="font-semibold">Work Days</span>
-              <span className="flex space-x-2">
-                <span className="flex items-center">
-                  <Check size={12} className="text-blue-500" />
-                  <span className="ml-1">Monday</span>
-                </span>
-                <span className="flex items-center">
-                  <Check size={12} className="text-blue-500" />
-                  <span className="ml-1">Tuesday</span>
-                </span>
-                <span className="flex items-center">
-                  <Check size={12} className="text-blue-500" />
-                  <span className="ml-1">Wednesday</span>
-                </span>
+          <div className="flex justify-between border-b border-gray-300 pb-2">
+            <span className="font-semibold">Specialty:</span>
+            <span>Cardiology</span>
+          </div>
+          <div className="flex justify-between border-b border-gray-300 pb-2 mt-2">
+            <span className="font-semibold">Degrees:</span>
+            <span>M.D. of Medicine</span>
+          </div>
+          <div className="flex justify-between border-b border-gray-300 pb-2 mt-2">
+            <span className="font-semibold">Training:</span>
+            <span>Lorem ipsum dolor sit...</span>
+          </div>
+          <div className="flex justify-between border-b border-gray-300 pb-2 mt-2">
+            <span className="font-semibold">Work Days</span>
+            <span className="flex space-x-2">
+              <span className="flex items-center">
+                <Check size={18} className="text-blue-500" />
+                <span className="ml-1">Monday</span>
               </span>
-            </div>
+              <span className="flex items-center">
+                <Check size={18} className="text-blue-500" />
+                <span className="ml-1">Tuesday</span>
+              </span>
+              <span className="flex items-center">
+                <Check size={18} className="text-blue-500" />
+                <span className="ml-1">Wednesday</span>
+              </span>
+            </span>
           </div>
         </div>
       </div>
